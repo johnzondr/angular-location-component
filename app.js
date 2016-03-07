c2999a6e96f7137ed9a5e0f2c29f1f78afb665e2
John Zhao <john@zondr.com>
2016-03-07 13:53:26 -0600

    processed and sorted location data

--- app.js
+++ app.js
@@ -13,9 +13,40 @@ angular.module('locationComponent', ['ngRoute', 'ngResource'])
 
 // service
 .service('locationService', function($http) {
-	// this.locations = [];
-
+	this.locations = [];
 	this.load = $http.get('/locations.json');
+	this.process = function(locations) {
+		return locations.sort(function(a, b){
+		    var keyA = a.state,
+		        keyB = b.state;
+		    // Compare the 2 dates
+		    if(keyA < keyB) return -1;
+		    if(keyA > keyB) return 1;
+		    return 0;
+		});
+	};
+	this.stateLocations = function(locations) {
+		var statesArray = []
+		var stateItemsArray = [];
+		var lastState = null;
+		locations.map(function(locationItem){
+			
+			if (lastState === null) {
+				stateItemsArray.push(locationItem)
+				lastState = locationItem.state
+			} else if (locationItem.state === lastState) {
+				stateItemsArray.push(locationItem)
+			} else if (locationItem.state != lastState){
+				lastState = locationItem.state
+				statesArray.push(stateItemsArray)
+				stateItemsArray = [locationItem]
+			}
+
+		});
+		statesArray.push(stateItemsArray);
+
+		return statesArray
+	};
 
 })
 
@@ -26,13 +57,45 @@ angular.module('locationComponent', ['ngRoute', 'ngResource'])
 .controller('homeController', ['$scope', 'locationService', 'filterFilter',
 	function($scope, locationService, filterFilter){
 	$scope.locations = [];
+	$scope.states = [];
+	$scope.selected = [];
 
 	locationService.load.then(function(locations){
-		$scope.locations = locations.data		
+		$scope.locations = locationService.process(locations.data);
 		// $scope.locations = angular.fromJson(locations.data);
-	})
+		$scope.states = locationService.stateLocations(locations.data);
+
+	});
+
+	$scope.$watch('locations', function(newVal) {
+		locationService.locations = newVal;
+	});
+
+	// $scope.$watch('selected', function(newVal) {
+	// 	console.log('changed')
+	// }, true);
 
 
+
+}])
+
+.controller('stateContainerController',['$scope', 
+	function($scope){
+
+	// toggle selection for a given fruit by name
+	  $scope.toggleSelection = function toggleSelection(locationId) {
+	    var idx = $scope.selected.indexOf(locationId);
+
+	    // is currently selected
+	    if (idx > -1) {
+	      $scope.selected.splice(idx, 1);
+	    }
+
+	    // is newly selected
+	    else {
+	      $scope.selected.push(locationId);
+	    }
+  };
 
 
 }])
@@ -48,7 +111,17 @@ angular.module('locationComponent', ['ngRoute', 'ngResource'])
 
 	}
 })
-
+.directive('stateContainer', function(){
+	return {
+		scope: {
+			stateLocation: '=stateLocation',
+			selected:'='
+		},
+		templateUrl: 'directives/stateContainer.html',
+		replace: true,
+		controller: 'stateContainerController'
+	}
+})
 
 ;
 
